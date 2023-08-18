15dd4ed4650dc0683cbbe17b79330150b6cd4ac0
mostafarakib <mostafa.rakib.10@gmail.com>
2023-08-19 01:24:40 +0600

    added import functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Papa from "papaparse";
+import "./App.css";
+import DataTable from "./components/DataTable";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    Papa.parse(file, {
+      header: true,
+      complete: (results) => {
+        setData(results.data);
+      },
+    });
+  };
+  console.log(data);
+  useEffect(() => {}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CSV Data Manipulation App</h1>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      {data.length ? <DataTable data={data}></DataTable> : null}
     </div>
   );
 }
---
+++ src/components/ProjectInfo.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProjectInfo = () => {
+  return (
+    <form>
+      <label>
+        Project Name
+        <input type="text" name="" />
+      </label>
+      <label>
+        Project Description
+        <input type="text" name="" />
+      </label>
+      <label>
+        Client
+        <input type="text" name="" />
+      </label>
+      <label>
+        Contractor
+        <input type="text" name="" />
+      </label>
+    </form>
+  );
+};
+
+export default ProjectInfo;
