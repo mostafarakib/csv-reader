5d0bbfe009e16a445c547303b50a6868a03b64bc
mostafarakib <mostafa.rakib.10@gmail.com>
2023-08-19 21:52:30 +0600

    added export table as pdf functionality

--- src/components/Result/DataTable.js
+++ src/components/Result/DataTable.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 
 const MemoizedInputCell = React.memo(({ value, onChange }) => {
   return (
@@ -13,43 +15,67 @@ const MemoizedInputCell = React.memo(({ value, onChange }) => {
 const DataTable = ({ data }) => {
   const [inputData, setInputData] = useState(data);
   const keys = Object.keys(data[0]);
+
   const handleInputChange = (index, key, value) => {
     const updatedData = [...inputData];
     updatedData[index][key] = value;
     setInputData(updatedData);
   };
+
+  const getTableBodyData = () => {
+    return data.map((dataObject) => {
+      const rowData = keys.map((key) => dataObject[key]);
+      return rowData;
+    });
+  };
+
+  const HandleExportPDF = () => {
+    const doc = new jsPDF();
+    const tableBodyData = getTableBodyData();
+    autoTable(doc, { head: [keys], body: tableBodyData });
+    doc.save("table.pdf");
+  };
   return (
-    <table className="table table-bordered">
-      <thead>
-        <tr>
-          {keys.map((key) => (
-            <th
-              style={{ backgroundColor: "#ECECEC" }}
-              className="px-2 py-1"
-              key={key}
-            >
-              {key}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((dataObject, index) => (
-          <tr key={index}>
+    <div style={{ position: "relative" }}>
+      <button
+        style={{ position: "absolute", right: "10px", top: "-50px" }}
+        className="btn btn-primary"
+        onClick={HandleExportPDF}
+      >
+        Download as PDF
+      </button>
+      <table id="csv-data-table" className="table table-bordered">
+        <thead>
+          <tr>
             {keys.map((key) => (
-              <td className="p-0" key={key}>
-                <MemoizedInputCell
-                  value={dataObject[key]}
-                  onChange={(e) =>
-                    handleInputChange(index, key, e.target.value)
-                  }
-                />
-              </td>
+              <th
+                style={{ backgroundColor: "#ECECEC" }}
+                className="px-2 py-1"
+                key={key}
+              >
+                {key}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((dataObject, index) => (
+            <tr key={index}>
+              {keys.map((key) => (
+                <td className="p-0" key={key}>
+                  <MemoizedInputCell
+                    value={dataObject[key]}
+                    onChange={(e) =>
+                      handleInputChange(index, key, e.target.value)
+                    }
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
